d8b2069f1d77083226ad58f0a0aa46b499cb9c57
Kell Banks <mckellbanks@gmail.com>
2020-04-08 17:12:21 -0600

    created html layout, added firebase config, global vars, current time function. Added moment.js to html page

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey            : 'AIzaSyB5oNsQ7FmhG_23Qw27Z4WSJdjh-7BbUFY',
+	authDomain        : 'kell-b.firebaseapp.com',
+	databaseURL       : 'https://kell-b.firebaseio.com',
+	projectId         : 'kell-b',
+	storageBucket     : 'kell-b.appspot.com',
+	messagingSenderId : '240187413865',
+	appId             : '1:240187413865:web:a0cd8bd2ba1420c60c19f8',
+	measurementId     : 'G-RCE79R6NCD'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var dataBase = firebase.dataBase();
+
+var trainName = '';
+var destination = '';
+var startTime = '';
+var frequency = 0;
+
+function currentTime() {
+	var current = moment().format('LT');
+	$('#currentTime').html(current);
+	setTimeout(currentTime, 1000);
+}
